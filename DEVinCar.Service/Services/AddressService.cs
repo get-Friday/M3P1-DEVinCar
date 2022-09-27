c546147737d83e8ec9b73b293c89f0f0314a5c0f
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 16:02:12 -0300

    Refactor: Cria AddressService

---
+++ DEVinCar.Service/Services/AddressService.cs
@@ -0,0 +1,28 @@
+﻿using DEVinCar.Service.DTOs;
+using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Interfaces.Services;
+
+namespace DEVinCar.Service.Services
+{
+    internal class AddressService : IAddressService
+    {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressService(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        public IList<AddressDTO> Get()
+        {
+            throw new NotImplementedException();
+        }
+        public void Alter(AddressDTO car)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(AddressDTO address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
