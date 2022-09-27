0d2a7efab253ab35698e6bfd0a5b250e34b709f0
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 17:12:30 -0300

    Refactor: Adiciona props para possibilitar mapeamento do DeliveryDTO

--- DEVinCar.Service/DTOs/DeliveryDTO.cs
+++ DEVinCar.Service/DTOs/DeliveryDTO.cs
@@ -2,6 +2,8 @@ namespace DEVinCar.Service.DTOs;
 
 public class DeliveryDTO
 {
-    public int? AddressId { get; set; }
+    public int Id { get; internal set; }
     public DateTime? DeliveryForecast { get; set; }
+    public int? AddressId { get; set; }
+    public int SaleId { get; internal set; }
 }
