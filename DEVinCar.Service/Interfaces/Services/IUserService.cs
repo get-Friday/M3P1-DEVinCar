6a4a8efb4d77108e11c38fc067c0848d48dfcf0c
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:31:01 -0300

    Refactor: Implementa interface UserService

---
+++ DEVinCar.Service/Interfaces/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Interfaces.Services
+{
+    public interface IUserService
+    {
+        IList<UserDTO> Get();
+        UserDTO GetById(int id);
+        IList<UserDTO> GetByIdbuy(int userId);
+        IList<UserDTO> GetSalesBySellerId(int userId);
+        void Post(UserDTO user);
+        void PostSaleUserId(SaleDTO sale);
+        void PostBuyUserId(SaleDTO buy);
+        void Delete(UserDTO user);
+    }
+}
