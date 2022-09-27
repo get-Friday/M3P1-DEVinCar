36e1b0a33161bb0987eca8a7b0e353db5ae547e1
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 17:20:20 -0300

    Refactor: Mapeia model para DTO State

--- DEVinCar.Service/DTOs/StateDTO.cs
+++ DEVinCar.Service/DTOs/StateDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DEVinCar.Service.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace DEVinCar.Service.DTOs
 {
@@ -11,5 +12,15 @@ namespace DEVinCar.Service.DTOs
         [Required(ErrorMessage = "The initiais is required.")]
         [MaxLength(2, ErrorMessage = "State initials must be a maximum of 2 characters.")]
         public string Initials { get; set; }
+
+        public StateDTO()
+        {
+        }
+        public StateDTO(State state)
+        {
+            Id = state.Id;
+            Name = state.Name;
+            Initials = state.Initials;
+        }
     }
 }
