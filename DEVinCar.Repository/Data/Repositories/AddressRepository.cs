243b0c934eed286ccea56364a88c43186beb1455
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:31:02 -0300

    Refactor: Implementa AddressRepository

---
+++ DEVinCar.Repository/Data/Repositories/AddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Models;
+
+namespace DEVinCar.Repository.Data.Repositories
+{
+    public class AddressRepository : BaseRepository<Address, int>, IAddressRepository
+    {
+        public AddressRepository(DevInCarDbContext context) : base(context)
+        {
+        }
+    }
+}
