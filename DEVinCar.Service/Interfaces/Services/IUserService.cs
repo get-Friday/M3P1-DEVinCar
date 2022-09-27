03a6a98e34faa1ad9fa48ceea46ce75880a95b4a
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 18:47:49 -0300

    Fix: Renomeia métodos e altera retorno de IUserService

--- DEVinCar.Service/Interfaces/Services/IUserService.cs
+++ DEVinCar.Service/Interfaces/Services/IUserService.cs
@@ -6,8 +6,8 @@ namespace DEVinCar.Service.Interfaces.Services
     {
         IList<UserDTO> Get();
         UserDTO GetById(int id);
-        IList<UserDTO> GetByIdbuy(int userId);
-        IList<UserDTO> GetSalesBySellerId(int userId);
+        IList<SaleDTO> GetSalesByUserId(int userId);
+        IList<SaleDTO> GetSalesBySellerId(int userId);
         void Post(UserDTO user);
         void PostSaleUserId(SaleDTO sale);
         void PostBuyUserId(SaleDTO buy);
