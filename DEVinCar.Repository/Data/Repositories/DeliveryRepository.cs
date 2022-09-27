294a1074449573e11afcbddfdaf88fd8c9c5b3a9
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:33:42 -0300

    Refactor: Implementa DeliveryRepository

---
+++ DEVinCar.Repository/Data/Repositories/DeliveryRepository.cs
@@ -0,0 +1,12 @@
+﻿using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Models;
+
+namespace DEVinCar.Repository.Data.Repositories
+{
+    public class DeliveryRepository : BaseRepository<Delivery, int>, IDeliveryRepository
+    {
+        public DeliveryRepository(DevInCarDbContext context) : base(context)
+        {
+        }
+    }
+}
