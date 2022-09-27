8b9cc04e5e87578f130604a52f3292add7aa2492
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:25:57 -0300

    Refactor: Implementa Interface CarRepository

---
+++ DEVinCar.Service/Interfaces/Repositories/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface ICarRepository
+    {
+        IEnumerable<Car> Get();
+        Car GetById(int id);
+        void Post(Car car);
+        void Alter(Car car);
+        void Delete(int id);
+    }
+}
