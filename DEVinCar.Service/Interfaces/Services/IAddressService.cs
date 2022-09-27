986bbd843a65c8159714c52ada79d9c691ac2acb
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:35:03 -0300

    Refactor: Implementa interface AddressService

---
+++ DEVinCar.Service/Interfaces/Services/IAddressService.cs
@@ -0,0 +1,11 @@
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Interfaces.Services
+{
+    public interface IAddressService
+    {
+        IList<AddressDTO> Get();
+        void Alter(AddressDTO car);
+        void Delete(AddressDTO address);
+    }
+}
