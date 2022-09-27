de95f29b14b63cfdeb960343615df6f826662b36
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:21:16 -0300

    Refactor: Implementa UserRepository

---
+++ DEVinCar.Repository/Data/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Models;
+
+namespace DEVinCar.Repository.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User, int>, IUserRepository
+    {
+        public UserRepository(DevInCarDbContext context) : base(context)
+        {
+        }
+        public IEnumerable<Sale> GetByIdbuy(int userId)
+        {
+            return _context.Sales.Where(s => s.BuyerId == userId);
+        }
+        public IEnumerable<Sale> GetSalesBySellerId(int userId)
+        {
+            return _context.Sales.Where(s => s.SellerId == userId);
+        }
+        public void PostSaleUserId(Sale sale)
+        {
+            _context.Sales.Add(sale);
+            _context.SaveChanges();
+        }
+        public void PostBuyUserId(Sale buy)
+        {
+            _context.Sales.Add(buy);
+            _context.SaveChanges();
+        }
+    }
+}
