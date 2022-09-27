67c020a259fbddd3e4082feb1b8ccf4e1c3bee10
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:39:45 -0300

    Refactor: Implementa interface SaleService

---
+++ DEVinCar.Service/Interfaces/Services/ISaleService.cs
@@ -0,0 +1,13 @@
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Interfaces.Services
+{
+    public interface ISaleService
+    {
+        IList<SaleDTO> GetItemsSale(int saleId);
+        void PostSale(SaleCarDTO saleCar);
+        void PostDelivery(DeliveryDTO delivery);
+        void AlterCarAmount(SaleCarDTO salesCar);
+        void AlterUnitPrice(SaleCarDTO salesCar);
+    }
+}
