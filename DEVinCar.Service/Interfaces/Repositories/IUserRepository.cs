5d9cafa4692064fe0e946146fb1d00f0867434d4
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:21:10 -0300

    Refactor: Implementa Interface UserRepository

---
+++ DEVinCar.Service/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface IUserRepository
+    {
+        IEnumerable<User> Get();
+        User GetById(int id);
+        IEnumerable<Sale> GetByIdbuy(int userId);
+        IEnumerable<Sale> GetSalesBySellerId(int userId);
+        void Post(User user);
+        void PostSaleUserId(Sale sale);
+        void PostBuyUserId(Sale buy);
+        void Delete(int userId);
+    }
+}
