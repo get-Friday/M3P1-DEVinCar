a469f638a20c67dd34d2bc2c1684db76415e181a
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:22:46 -0300

    Refactor: Implementa StateRepository

---
+++ DEVinCar.Repository/Data/Repositories/StateRepository.cs
@@ -0,0 +1,29 @@
+﻿using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Models;
+
+namespace DEVinCar.Repository.Data.Repositories
+{
+    public class StateRepository : BaseRepository<State, int>, IStateRepository
+    {
+        public StateRepository(DevInCarDbContext context) : base(context)
+        {
+        }
+        public void PostAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+        }
+        public City GetCityById(int cityId)
+        {
+            return _context.Cities.Find(cityId);
+        }
+        public State GetStateById(int stateId)
+        {
+            return _context.States.Find(stateId);
+        }
+        public IEnumerable<City> GetCitiesByStateId(int stateId)
+        {
+            return _context.Cities.Where(c => c.StateId == stateId);
+        }
+    }
+}
