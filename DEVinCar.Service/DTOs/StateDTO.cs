328e329461f0f27de210f3d0591a8141d30c696d
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 17:12:49 -0300

    Refactor: Adiciona props para possibilitar mapeamento do StateDTO

--- DEVinCar.Service/DTOs/StateDTO.cs
+++ DEVinCar.Service/DTOs/StateDTO.cs
@@ -4,6 +4,7 @@ namespace DEVinCar.Service.DTOs
 {
     public class StateDTO
     {
+        public int Id { get; internal set; }
         [Required(ErrorMessage = "The Name is required.")]
         [MaxLength(100, ErrorMessage = "State name must be a maximum of 100 characters.")]
         public string Name { get; set; }
