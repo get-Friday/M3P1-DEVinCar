82fb614c016e94c1f31477128330e7b30fffc027
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 09:14:59 -0300

    Refactor: Move entidade User para configuração

--- DEVinCar.Repository/Data/DevInCarDbContext.cs
+++ DEVinCar.Repository/Data/DevInCarDbContext.cs
@@ -38,36 +38,7 @@ public class DevInCarDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new CityMap());
-
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.ToTable("Users");
-            entity.HasKey(u => u.Id);
-            entity
-                .Property(u => u.Email)
-                .HasMaxLength(150)
-                .IsRequired();
-
-            entity
-                .Property(u => u.Password)
-                .HasMaxLength(50)
-                .IsRequired();
-
-            entity
-                .Property(u => u.Name)
-                .HasMaxLength(255)
-                .IsRequired();
-
-            entity
-                .Property(u => u.BirthDate);
-            entity
-                .HasData(new[] {
-                    new User (1, "jose@email.com", "123456opp78", "Jose", new DateTime(2000, 12, 10)),
-                    new User (2, "andrea@email.com", "987dasd654321", "Andrea", new DateTime(1999, 05, 11)),
-                    new User (3, "adao@email.com", "2589asd", "Adao", new DateTime(2005, 09, 02)),
-                    new User (4, "monique@email.com", "asd45uio", "Monique", new DateTime(2001, 06, 07)),
-                });
-        });
+        modelBuilder.ApplyConfiguration(new UserMap());
 
         modelBuilder.Entity<Car>(entity =>
         {
