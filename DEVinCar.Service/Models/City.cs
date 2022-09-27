1c390c1b89c70e8a835b32f65eb4dc4ad2043817
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 17:11:12 -0300

    Refactor: Mapeia o DTO para model City

--- DEVinCar.Service/Models/City.cs
+++ DEVinCar.Service/Models/City.cs
@@ -1,12 +1,24 @@
-﻿namespace DEVinCar.Service.Models
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Models
 {
     public class City
     {
         public int Id { get; set; }
         public int StateId { get; set; }
         public string Name { get; set; }
-        public virtual State State { get; set; }
 
+        public virtual State State { get; set; }
         public virtual List<Address> Addresses { get; set; }
+
+        public City()
+        {
+        }
+        public City(CityDTO city)
+        {
+            Id = city.Id;
+            StateId = city.StateId;
+            Name = city.Name;
+        }
     }
 }
