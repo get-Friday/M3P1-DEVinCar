5bb6c7cd3d35289a348dc74f338cd0607c7348c3
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 09:52:48 -0300

    Refactor: Implementa métodos da BaseRepository

--- DEVinCar.Repository/Data/Repositories/BaseRepository.cs
+++ DEVinCar.Repository/Data/Repositories/BaseRepository.cs
@@ -7,5 +7,28 @@
         {
             _context = context;
         }
+        public IEnumerable<TEntity> Get(TEntity entity)
+        {
+            return _context.Set<TEntity>();
+        }
+        public TEntity GetById(TKey key)
+        {
+            return _context.Set<TEntity>().Find(key);
+        }
+        public void Insert(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+        public void Edit(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+        }
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
     }
 }
