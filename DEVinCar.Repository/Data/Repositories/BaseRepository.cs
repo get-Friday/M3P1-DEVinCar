6d190e99ed94de619c11a5f722ed85c41f8072ff
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 09:55:17 -0300

    Fix: Altera escopo de BaseRepository para public

--- DEVinCar.Repository/Data/Repositories/BaseRepository.cs
+++ DEVinCar.Repository/Data/Repositories/BaseRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace DEVinCar.Repository.Data.Repositories
 {
-    internal class BaseRepository <TEntity, TKey> where TEntity : class
+    public class BaseRepository <TEntity, TKey> where TEntity : class
     {
         protected readonly DevInCarDbContext _context;
         public BaseRepository(DevInCarDbContext context)
