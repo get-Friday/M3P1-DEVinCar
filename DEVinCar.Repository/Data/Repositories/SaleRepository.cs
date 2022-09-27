cf2dbc8a37e75ee0261a66c0d2cfba75de1883b2
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:54:26 -0300

    Refactor: Implementa SaleRepository

---
+++ DEVinCar.Repository/Data/Repositories/SaleRepository.cs
@@ -0,0 +1,41 @@
+﻿using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEVinCar.Repository.Data.Repositories
+{
+    internal class SaleRepository : BaseRepository<Sale, int>, ISaleRepository
+    {
+        public SaleRepository(DevInCarDbContext context) : base(context)
+        {
+        }
+        public IEnumerable<Sale> GetItemsSale(int saleId)
+        {
+            return _context.Sales
+                .Include(s => s.Cars)
+                .Include(s => s.UserBuyer)
+                .Include(s => s.UserSeller)
+                .Where(s => s.Id == saleId);
+        }
+        public void PostSale(SaleCar saleCar)
+        {
+            _context.SaleCars.Add(saleCar);
+            _context.SaveChanges();
+        }
+        public void PostDelivery(Delivery delivery)
+        {
+            _context.Deliveries.Add(delivery);
+            _context.SaveChanges();
+        }
+        public void AlterCarAmount(SaleCar salesCar)
+        {
+            _context.SaleCars.Update(salesCar);
+            _context.SaveChanges();
+        }
+        public void AlterUnitPrice(SaleCar salesCar)
+        {
+            _context.SaleCars.Update(salesCar);
+            _context.SaveChanges();
+        }
+    }
+}
