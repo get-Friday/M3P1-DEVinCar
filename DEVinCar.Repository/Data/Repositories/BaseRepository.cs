c6612f79dc539c919d72aeecee004b06a8fc435f
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 09:42:04 -0300

    Refactor: Cria BaseRepository

---
+++ DEVinCar.Repository/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DEVinCar.Repository.Data.Repositories
+{
+    internal class BaseRepository <TEntity, TKey> where TEntity : class
+    {
+        protected readonly DevInCarDbContext _context;
+        public BaseRepository(DevInCarDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
