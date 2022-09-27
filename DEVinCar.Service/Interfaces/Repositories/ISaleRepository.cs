df850ea34913f581e478a59d655d32bdbab08d35
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:54:21 -0300

    Refactor: Implementa Interface SaleRepository

---
+++ DEVinCar.Service/Interfaces/Repositories/ISaleRepository.cs
@@ -0,0 +1,14 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface ISaleRepository
+    {
+        IEnumerable<Sale> GetItemsSale(int saleId);
+        void PostSale(SaleCar saleCar);
+        void PostDelivery(Delivery delivery);
+        void AlterCarAmount(SaleCar salesCar);
+        void AlterUnitPrice(SaleCar salesCar);
+
+    }
+}
