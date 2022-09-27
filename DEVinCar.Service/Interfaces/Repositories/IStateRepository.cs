29f0deceef3feb6db324f329969fd5321763ccf7
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:22:40 -0300

    Refactor: Implementa interface StateRepository

---
+++ DEVinCar.Service/Interfaces/Repositories/IStateRepository.cs
@@ -0,0 +1,14 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface IStateRepository
+    {
+        IEnumerable<State> Get();
+        void Post(State state);
+        void PostAddress(Address address);
+        City GetCityById(int cityId);
+        State GetStateById(int stateId);
+        IEnumerable<City> GetCitiesByStateId(int stateId);
+    }
+}
