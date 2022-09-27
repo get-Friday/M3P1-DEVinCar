297f341677ad1b3c40c5d644863094521021e4bc
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 18:40:02 -0300

    Refactor: Implementa métodos em StateService

--- DEVinCar.Service/Services/StateService.cs
+++ DEVinCar.Service/Services/StateService.cs
@@ -1,6 +1,8 @@
 ﻿using DEVinCar.Service.DTOs;
 using DEVinCar.Service.Interfaces.Repositories;
 using DEVinCar.Service.Interfaces.Services;
+using DEVinCar.Service.Models;
+using DEVinCar.Service.ViewModels;
 
 namespace DEVinCar.Service.Services
 {
@@ -14,27 +16,53 @@ namespace DEVinCar.Service.Services
         }
         public IList<StateDTO> Get()
         {
-            throw new NotImplementedException();
+            return _stateRepository.Get()
+                .Select(s => new StateDTO(s))
+                .ToList();
         }
         public void Post(StateDTO state)
         {
-            throw new NotImplementedException();
+            _stateRepository.Post(new State(state));
         }
         public void PostAddress(AddressDTO address)
         {
-            throw new NotImplementedException();
+            _stateRepository.PostAddress(new Address(address));
         }
-        public CityDTO GetCityById(int cityId)
+        public GetCityByIdViewModel GetCityById(int cityId)
         {
-            throw new NotImplementedException();
+            City city = _stateRepository.GetCityById(cityId);
+            State state = _stateRepository.GetStateById(city.StateId);
+
+            return new GetCityByIdViewModel(
+                city.Id,
+                city.Name,
+                state.Id,
+                state.Name,
+                state.Initials
+            );
         }
-        public StateDTO GetStateById(int stateId)
+        public GetStateViewModel GetStateById(int stateId)
         {
-            throw new NotImplementedException();
+            State state = _stateRepository.GetStateById(stateId);
+
+            GetStateViewModel stateViewModel = new(
+                state.Id,
+                state.Name,
+                state.Initials
+            )
+            {
+                Cities = _stateRepository.GetCitiesByStateId(stateId)
+                    .Select(c => c.Name)
+                    .ToList()
+            };
+
+            return stateViewModel;
         }
-        public IEnumerable<CityDTO> GetCitiesByStateId(int stateId)
+        public IList<CityDTO> GetCitiesByStateId(int stateId)
         {
-            throw new NotImplementedException();
+            return _stateRepository.GetCitiesByStateId(stateId)
+                .Select(c => new CityDTO(c))
+                .ToList();
         }
     }
 }
