4b349a046f372cf960d879bb2dd568e340033f97
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 09:22:41 -0300

    Refactor: Cria Map para entidade SaleCar

---
+++ DEVinCar.Repository/Data/Mappings/SaleCarMap.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DEVinCar.Service.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DEVinCar.Repository.Data.Mappings
+{
+    internal class SaleCarMap : IEntityTypeConfiguration<SaleCar>
+    {
+        public void Configure(EntityTypeBuilder<SaleCar> builder)
+        {
+            builder.ToTable("SaleCars");
+            builder.HasKey(sc => sc.Id);
+            builder.Property(sc => sc.Id)
+                .HasColumnType("int");
+
+            builder.Property(sc => sc.SaleId)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(sc => sc.CarId)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(sc => sc.UnitPrice)
+                .HasPrecision(18, 2);
+
+            builder.Property(sc => sc.Amount)
+                .HasColumnType("int");
+
+            builder.HasOne<Car>(c => c.Car)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(c => c.Id);
+
+            builder.HasOne<Sale>(s => s.Sale)
+                .WithMany(c => c.Cars)
+                .HasForeignKey(s => s.Id);
+        }
+    }
+}
