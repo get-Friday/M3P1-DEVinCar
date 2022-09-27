9806b159444862fdc1f950123eb803d51f440e59
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:41:08 -0300

    Refactor: Implementa interface StateService

---
+++ DEVinCar.Service/Interfaces/Services/IStateService.cs
@@ -0,0 +1,14 @@
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Interfaces.Services
+{
+    public interface IStateService
+    {
+        IList<StateDTO> Get();
+        void Post(StateDTO state);
+        void PostAddress(AddressDTO address);
+        CityDTO GetCityById(int cityId);
+        StateDTO GetStateById(int stateId);
+        IEnumerable<CityDTO> GetCitiesByStateId(int stateId);
+    }
+}
