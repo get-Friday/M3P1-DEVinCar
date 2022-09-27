e91a47f83ab484b4d7a821907bf23e9d50eb4fa2
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 16:10:10 -0300

    Refactor: Implementa classe de injeção de dependência

--- DEVinCar.DI/DI.cs
+++ DEVinCar.DI/DI.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DEVinCar.Repository.Data;
+using DEVinCar.Repository.Data.Repositories;
+using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Interfaces.Services;
+using DEVinCar.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DEVinCar.DI
 {
@@ -6,6 +11,19 @@ namespace DEVinCar.DI
     {
         public static IServiceCollection Register(this IServiceCollection services)
         {
+            services.AddDbContext<DevInCarDbContext>();
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<ICarRepository, CarRepository>();
+            services.AddScoped<ICarService, CarService>();
+            services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+            services.AddScoped<IDeliveryService, DeliveryService>();
+            services.AddScoped<ISaleRepository, SaleRepository>();
+            services.AddScoped<ISaleService, SaleService>();
+            services.AddScoped<IStateRepository, StateRepository>();
+            services.AddScoped<IStateService, StateService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
             return services;
         }
     }
