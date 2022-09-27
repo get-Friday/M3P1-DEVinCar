a09d69940af36a246a709327fe16a577d982bbd8
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 16:09:00 -0300

    Refactor: Cria StateService

---
+++ DEVinCar.Service/Services/StateService.cs
@@ -0,0 +1,40 @@
+﻿using DEVinCar.Service.DTOs;
+using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Interfaces.Services;
+
+namespace DEVinCar.Service.Services
+{
+    internal class StateService : IStateService
+    {
+        private readonly IStateRepository _stateRepository;
+
+        public StateService(IStateRepository stateRepository)
+        {
+            _stateRepository = stateRepository;
+        }
+        public IList<StateDTO> Get()
+        {
+            throw new NotImplementedException();
+        }
+        public void Post(StateDTO state)
+        {
+            throw new NotImplementedException();
+        }
+        public void PostAddress(AddressDTO address)
+        {
+            throw new NotImplementedException();
+        }
+        public CityDTO GetCityById(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+        public StateDTO GetStateById(int stateId)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<CityDTO> GetCitiesByStateId(int stateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
