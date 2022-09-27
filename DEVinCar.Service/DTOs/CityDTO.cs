a7def68b464874fce7e1fbd2143629c586202e17
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 17:12:24 -0300

    Refactor: Adiciona props para possibilitar mapeamento do CityDTO

--- DEVinCar.Service/DTOs/CityDTO.cs
+++ DEVinCar.Service/DTOs/CityDTO.cs
@@ -4,11 +4,10 @@ namespace DEVinCar.Service.DTOs
 {
     public class CityDTO
     {
+        public int Id { get; internal set; }
+        public int StateId { get; internal set; }
         [Required(ErrorMessage = "The name is required")]
         [MaxLength(255)]
         public string Name { get; set; }
-
-
-
     }
 }
