52edf6985ba1869b02235b78a9579a24a4ca4ef1
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 15:36:37 -0300

    Refactor: Implementa interface CarService

---
+++ DEVinCar.Service/Interfaces/Services/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using DEVinCar.Service.DTOs;
+
+namespace DEVinCar.Service.Interfaces.Services
+{
+    public interface ICarService
+    {
+        IList<CarDTO> Get();
+        CarDTO GetById(int id);
+        void Post(CarDTO car);
+        void Alter(CarDTO car);
+        void Delete(CarDTO car);
+    }
+}
