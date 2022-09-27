d77c6ea87b6c428c802231041dba7f64dcf415c7
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 16:05:19 -0300

    Refactor: Cria DeliveryService

---
+++ DEVinCar.Service/Services/DeliveryService.cs
@@ -0,0 +1,20 @@
+﻿using DEVinCar.Service.DTOs;
+using DEVinCar.Service.Interfaces.Repositories;
+using DEVinCar.Service.Interfaces.Services;
+
+namespace DEVinCar.Service.Services
+{
+    internal class DeliveryService : IDeliveryService
+    {
+        private readonly IDeliveryRepository _DeliveryRepository;
+
+        public DeliveryService(IDeliveryRepository deliveryRepository)
+        {
+            _DeliveryRepository = deliveryRepository;
+        }
+        public IList<DeliveryDTO> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
