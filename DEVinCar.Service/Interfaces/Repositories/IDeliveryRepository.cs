f8392ab7b116b21c578df9ec24ddd177b938a424
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-27 10:33:37 -0300

    Refactor: Implementa Interface DeliveryRepository

---
+++ DEVinCar.Service/Interfaces/Repositories/IDeliveryRepository.cs
@@ -0,0 +1,9 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface IDeliveryRepository
+    {
+        IEnumerable<Delivery> Get();
+    }
+}
