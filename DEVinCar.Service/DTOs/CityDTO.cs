45fc238d84e21827e64c552b322a9687f3ff092e
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 15:58:16 -0300

    Fix: CityDTO remove internal set

--- DEVinCar.Service/DTOs/CityDTO.cs
+++ DEVinCar.Service/DTOs/CityDTO.cs
@@ -6,7 +6,7 @@ namespace DEVinCar.Service.DTOs
     public class CityDTO
     {
         public int Id { get; internal set; }
-        public int StateId { get; internal set; }
+        public int StateId { get; set; }
         [Required(ErrorMessage = "The name is required")]
         [MaxLength(255)]
         public string Name { get; set; }
