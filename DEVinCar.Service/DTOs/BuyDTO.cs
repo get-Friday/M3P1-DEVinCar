c853f3245050372dc74165bba88085b4a72f1981
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 09:53:37 -0300

    Fix: Adiciona ctor e props restantes do BuyDTO

--- DEVinCar.Service/DTOs/BuyDTO.cs
+++ DEVinCar.Service/DTOs/BuyDTO.cs
@@ -1,12 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DEVinCar.Service.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace DEVinCar.Service.DTOs
 {
     public class BuyDTO
     {
+        public int Id { get; internal set; }
+        public DateTime SaleDate { get; set; }
+        public int BuyerId { get; set; }
         [Required(ErrorMessage = "The SelleId is required.")]
         public int SellerId { get; set; }
-        public DateTime SaleDate { get; set; }
+
+        public BuyDTO()
+        {
+        }
+        public BuyDTO(Sale sale)
+        {
+            Id = sale.Id;
+            SaleDate = sale.SaleDate;
+            BuyerId = sale.BuyerId;
+            SellerId = sale.SellerId;
+        }
     }
 }
 
