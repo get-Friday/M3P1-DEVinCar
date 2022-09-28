6547b5dd6e700438aea574213ea1b3080607e01a
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 15:59:03 -0300

    Fix: IStateRepository altera métodos

--- DEVinCar.Service/Interfaces/Repositories/IStateRepository.cs
+++ DEVinCar.Service/Interfaces/Repositories/IStateRepository.cs
@@ -5,10 +5,11 @@ namespace DEVinCar.Service.Interfaces.Repositories
     internal interface IStateRepository
     {
         IEnumerable<State> Get();
-        void Post(State state);
-        void PostAddress(Address address);
+        IEnumerable<State> GetByName(string name);
         City GetCityById(int cityId);
         State GetStateById(int stateId);
+        void PostCity(City city);
+        void PostAddress(Address address);
         IEnumerable<City> GetCitiesByStateId(int stateId);
     }
 }
