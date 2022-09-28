a9fd30647575d7cf11c1befdb32caa34f155d107
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 20:27:53 -0300

    Fix: Insere parâmetros de regra em Get ICarService

--- DEVinCar.Service/Interfaces/Services/ICarService.cs
+++ DEVinCar.Service/Interfaces/Services/ICarService.cs
@@ -4,7 +4,7 @@ namespace DEVinCar.Service.Interfaces.Services
 {
     public interface ICarService
     {
-        IList<CarDTO> Get();
+        IList<CarDTO> Get(string? name, decimal? priceMin, decimal? priceMax);
         CarDTO GetById(int id);
         void Post(CarDTO car);
         void Alter(CarDTO car);
