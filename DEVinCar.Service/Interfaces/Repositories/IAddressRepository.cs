05beace3afa74c76fa7ca294e37180b7731c74f0
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 20:37:16 -0300

    Fix: Retorno Get para IQueryable em IAdressRepository

--- DEVinCar.Service/Interfaces/Repositories/IAddressRepository.cs
+++ DEVinCar.Service/Interfaces/Repositories/IAddressRepository.cs
@@ -4,7 +4,7 @@ namespace DEVinCar.Service.Interfaces.Repositories
 {
     internal interface IAddressRepository
     {
-        IEnumerable<Address> GetWithCity();
+        IQueryable<Address> GetIncludeCity();
         Address GetById(int id);
         void Alter(Address car);
         void Delete(Address address);
