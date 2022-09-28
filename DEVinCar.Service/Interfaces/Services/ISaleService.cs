57a3564534108050fe9783dd509ed5c1cbf1c637
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 14:56:20 -0300

    Fix: Move métodos do IUserService para ISaleService

--- DEVinCar.Service/Interfaces/Services/ISaleService.cs
+++ DEVinCar.Service/Interfaces/Services/ISaleService.cs
@@ -6,7 +6,11 @@ namespace DEVinCar.Service.Interfaces.Services
     public interface ISaleService
     {
         IList<SaleViewModel> GetItemsSale(int saleId);
+        IList<SaleDTO> GetSalesByUserId(int userId);
+        IList<SaleDTO> GetSalesBySellerId(int userId);
         void PostSale(SaleCarDTO saleCar);
+        void PostSaleUserId(SaleDTO sale);
+        void PostBuyUserId(BuyDTO buy);
         void PostDelivery(DeliveryDTO delivery);
         void Alter(SaleCarDTO salesCar);
         decimal GetSuggestedPrice(int carId);
--- DEVinCar.Service/Interfaces/Services/IUserService.cs
+++ DEVinCar.Service/Interfaces/Services/IUserService.cs
@@ -6,11 +6,7 @@ namespace DEVinCar.Service.Interfaces.Services
     {
         IList<UserDTO> Get();
         UserDTO GetById(int id);
-        IList<SaleDTO> GetSalesByUserId(int userId);
-        IList<SaleDTO> GetSalesBySellerId(int userId);
         void Post(UserDTO user);
-        void PostSaleUserId(SaleDTO sale);
-        void PostBuyUserId(BuyDTO buy);
         void Delete(int userId);
     }
 }
