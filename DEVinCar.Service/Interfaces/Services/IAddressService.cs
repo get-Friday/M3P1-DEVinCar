f1f70be771e733ca3c27fabbdea97f99a2f81ffd
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 20:37:41 -0300

    Fix: Retorno Get para IQueryable e parâmetros em IAdressService

--- DEVinCar.Service/Interfaces/Services/IAddressService.cs
+++ DEVinCar.Service/Interfaces/Services/IAddressService.cs
@@ -5,7 +5,7 @@ namespace DEVinCar.Service.Interfaces.Services
 {
     public interface IAddressService
     {
-        IList<AddressViewModel> Get();
+        IList<AddressViewModel> Get(int? cityId,int? stateId,string? street,string? cep);
         void Alter(AddressPatchDTO addressPatch);
         void Delete(int id);
     }
