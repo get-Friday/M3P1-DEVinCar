9bc0de1876acf91ab0b0afab422e7c093a4c2499
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 10:38:06 -0300

    Fix: Remove opcional em AddressId e internal set em SaleId em DeliveryDTO

--- DEVinCar.Service/DTOs/DeliveryDTO.cs
+++ DEVinCar.Service/DTOs/DeliveryDTO.cs
@@ -6,8 +6,8 @@ public class DeliveryDTO
 {
     public int Id { get; internal set; }
     public DateTime? DeliveryForecast { get; set; }
-    public int? AddressId { get; set; }
-    public int SaleId { get; internal set; }
+    public int AddressId { get; set; }
+    public int SaleId { get; set; }
 
     public DeliveryDTO()
     {
