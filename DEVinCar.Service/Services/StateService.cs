82bb50badfa7a0a7d9bdd114ee9d018c73348d33
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 15:59:35 -0300

    Refactor: StateService com métodos e TODO

--- DEVinCar.Service/Services/StateService.cs
+++ DEVinCar.Service/Services/StateService.cs
@@ -14,24 +14,68 @@ namespace DEVinCar.Service.Services
         {
             _stateRepository = stateRepository;
         }
-        public IList<StateDTO> Get()
+        public IList<GetStateViewModel> Get()
         {
             return _stateRepository.Get()
-                .Select(s => new StateDTO(s))
+                .Select(s => new GetStateViewModel(
+                        s.Id,
+                        s.Name,
+                        s.Initials
+                    )
+                    {
+                        Cities = _stateRepository.GetCitiesByStateId(s.Id)
+                            .Select(c => c.Name)
+                            .ToList()
+                    })
                 .ToList();
         }
-        public void Post(StateDTO state)
+        public IList<GetStateViewModel> GetByName(string name)
         {
-            _stateRepository.Post(new State(state));
+
+            return _stateRepository.GetByName(name)
+                .Select(s => new GetStateViewModel(
+                        s.Id,
+                        s.Name,
+                        s.Initials
+                    )
+                {
+                    Cities = _stateRepository.GetCitiesByStateId(s.Id)
+                            .Select(c => c.Name)
+                            .ToList()
+                })
+                .ToList();
         }
-        public void PostAddress(AddressDTO address)
+        // TODO
+        // Verificar se state == null retornar notfound
+        // Verificar se existe outra cidade com mesmo nome e state.Id retornar BadRequest
+        public void PostCity(CityDTO city)
         {
-            _stateRepository.PostAddress(new Address(address));
+            _stateRepository.PostCity(new City(city));
         }
-        public GetCityByIdViewModel GetCityById(int cityId)
+        // TODO
+        // Verificar se stateId ou cityId inexistentes retornar NotFound
+        // Verificar se cityId tenha um stateId diferente do enviado retornar BadRequest
+        public void PostAddress(int stateId, int cityId, AddressDTO addressDTO)
         {
             City city = _stateRepository.GetCityById(cityId);
-            State state = _stateRepository.GetStateById(city.StateId);
+            Address address = new()
+            {
+                CityId = cityId,
+                Street = addressDTO.Street,
+                Number = addressDTO.Number,
+                Cep = addressDTO.Cep,
+                Complement = addressDTO.Complement,
+                City = city
+            };
+            _stateRepository.PostAddress(address);
+        }
+        // TODO
+        // Verificar se stateId ou cityId inexistentes retornar NotFound
+        // Verificar se cityId tenha um stateId diferente do enviado retornar BadRequest
+        public GetCityByIdViewModel GetCityById(int stateId, int cityId)
+        {
+            City city = _stateRepository.GetCityById(cityId);
+            State state = _stateRepository.GetStateById(stateId);
 
             return new GetCityByIdViewModel(
                 city.Id,
@@ -58,10 +102,19 @@ namespace DEVinCar.Service.Services
 
             return stateViewModel;
         }
-        public IList<CityDTO> GetCitiesByStateId(int stateId)
+        // TODO
+        // Verificar se getCitiesByStateId.length == 0 return NoContent
+        public IList<GetCityByIdViewModel> GetCitiesByStateId(int stateId)
         {
+            State state = _stateRepository.GetStateById(stateId);
             return _stateRepository.GetCitiesByStateId(stateId)
-                .Select(c => new CityDTO(c))
+                .Select(c => new GetCityByIdViewModel(
+                    c.Id,
+                    c.Name,
+                    state.Id,
+                    state.Name,
+                    state.Initials
+                    ))
                 .ToList();
         }
     }
