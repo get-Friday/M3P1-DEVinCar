eb84dd76ccf4e9d7918b0f2a4cda2e08314d7844
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 15:58:52 -0300

    Fix: IStateService altera métodos

--- DEVinCar.Service/Interfaces/Services/IStateService.cs
+++ DEVinCar.Service/Interfaces/Services/IStateService.cs
@@ -5,11 +5,12 @@ namespace DEVinCar.Service.Interfaces.Services
 {
     public interface IStateService
     {
-        IList<StateDTO> Get();
-        void Post(StateDTO state);
-        void PostAddress(AddressDTO address);
-        GetCityByIdViewModel GetCityById(int cityId);
+        IList<GetStateViewModel> Get();
+        IList<GetStateViewModel> GetByName(string name);
+        void PostCity(CityDTO city);
+        void PostAddress(int stateId, int cityId, AddressDTO address);
+        GetCityByIdViewModel GetCityById(int stateId, int cityId);
         GetStateViewModel GetStateById(int stateId);
-        IList<CityDTO> GetCitiesByStateId(int stateId);
+        IList<GetCityByIdViewModel> GetCitiesByStateId(int stateId);
     }
 }
