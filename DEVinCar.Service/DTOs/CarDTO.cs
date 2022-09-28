7500f7944bdd8e48d1d1cd0de744c567f67abd04
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 16:18:13 -0300

    Fix: CarDTO remove internal set

--- DEVinCar.Service/DTOs/CarDTO.cs
+++ DEVinCar.Service/DTOs/CarDTO.cs
@@ -5,7 +5,7 @@ namespace DEVinCar.Service.DTOs
 {
     public class CarDTO
     {
-        public int Id { get; internal set; }
+        public int Id { get; set; }
         [Required(ErrorMessage = "The name is required")]
         [MaxLength(255)]
         public string Name { get; set; }
