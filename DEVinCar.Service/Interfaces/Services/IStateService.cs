06e98adf08adb6ac8e3360bc529b008d9bff4a54
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 21:06:20 -0300

    Fix: Adiciona parâmetros para métodos em IStateService

--- DEVinCar.Service/Interfaces/Services/IStateService.cs
+++ DEVinCar.Service/Interfaces/Services/IStateService.cs
@@ -5,12 +5,12 @@ namespace DEVinCar.Service.Interfaces.Services
 {
     public interface IStateService
     {
-        IList<GetStateViewModel> Get();
+        IList<GetStateViewModel> Get(string name);
         IList<GetStateViewModel> GetByName(string name);
         void PostCity(CityDTO city);
         void PostAddress(int stateId, int cityId, AddressDTO address);
         GetCityByIdViewModel GetCityById(int stateId, int cityId);
         GetStateViewModel GetStateById(int stateId);
-        IList<GetCityByIdViewModel> GetCitiesByStateId(int stateId);
+        IList<GetCityByIdViewModel> GetCitiesByStateId(int stateId, string? name);
     }
 }
