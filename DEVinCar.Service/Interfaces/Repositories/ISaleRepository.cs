03905814e0396d8fd5b7f5becdcaac0eb910a2b3
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-29 20:55:35 -0300

    Refactor: Move métodos não pertencentes a ISaleRepository

--- DEVinCar.Service/Interfaces/Repositories/ISaleRepository.cs
+++ DEVinCar.Service/Interfaces/Repositories/ISaleRepository.cs
@@ -7,12 +7,7 @@ namespace DEVinCar.Service.Interfaces.Repositories
         IEnumerable<Sale> GetItemsSale(int saleId);
         IEnumerable<Sale> GetSalesByUserId(int userId);
         IEnumerable<Sale> GetSalesBySellerId(int userId);
-        void PostSale(SaleCar saleCar);
-        void PostDelivery(Delivery delivery);
         void PostSaleUserId(Sale sale);
         void PostBuyUserId(Sale buy);
-        void Alter(SaleCar salesCar);
-        decimal GetSuggestedPrice(int carId);
-        SaleCar GetSoldCar(int carId);
     }
 }
