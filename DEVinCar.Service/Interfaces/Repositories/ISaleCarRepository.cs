97f7d92fc4ea5a57c3dbbb6fafddf73aebbdea69
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-29 20:53:32 -0300

    Refactor: Cria repositório para SaleCar

---
+++ DEVinCar.Service/Interfaces/Repositories/ISaleCarRepository.cs
@@ -0,0 +1,12 @@
+﻿using DEVinCar.Service.Models;
+
+namespace DEVinCar.Service.Interfaces.Repositories
+{
+    internal interface ISaleCarRepository
+    {
+        SaleCar GetSoldCar(int carId);
+        void Post(SaleCar saleCar);
+        void Alter(SaleCar salesCar);
+        bool HasBeenSold(int carId);
+    }
+}
