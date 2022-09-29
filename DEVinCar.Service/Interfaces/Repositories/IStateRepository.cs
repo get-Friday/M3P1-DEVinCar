d81562f4f1cef8731701e563825d73717a5d7eee
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-28 21:05:49 -0300

    Fix: Altera retornos para IQueryable em IStateRepository

--- DEVinCar.Service/Interfaces/Repositories/IStateRepository.cs
+++ DEVinCar.Service/Interfaces/Repositories/IStateRepository.cs
@@ -4,12 +4,12 @@ namespace DEVinCar.Service.Interfaces.Repositories
 {
     internal interface IStateRepository
     {
-        IEnumerable<State> Get();
+        IQueryable<State> Get();
         IEnumerable<State> GetByName(string name);
         City GetCityById(int cityId);
         State GetStateById(int stateId);
         void PostCity(City city);
         void PostAddress(Address address);
-        IEnumerable<City> GetCitiesByStateId(int stateId);
+        IQueryable<City> GetCitiesByStateId(int stateId);
     }
 }
