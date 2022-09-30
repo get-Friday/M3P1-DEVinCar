4fc456544df0e9f99ed96a818081d67a34d80e04
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-30 15:50:49 -0300

    Feat: Cria classe InvalidObjectManipulationException

---
+++ DEVinCar.Service/Exceptions/InvalidObjectManipulationException.cs
@@ -0,0 +1,9 @@
+﻿namespace DEVinCar.Service.Exceptions
+{
+    public class InvalidObjectManipulationException : Exception
+    {
+        public InvalidObjectManipulationException(string? message) : base(message)
+        {
+        }
+    }
+}
