d22d887d5c7535d3d61e79a2f58cf8163757be13
get.Friday <gabriel.getdev@gmail.com>
2022-10-03 21:45:06 -0300

    Feat: Adiciona construtor ao LoginDTO

--- DEVinCar.Service/DTOs/LoginDTO.cs
+++ DEVinCar.Service/DTOs/LoginDTO.cs
@@ -1,4 +1,5 @@
 ﻿using DEVinCar.Service.Enums;
+using DEVinCar.Service.Models;
 
 namespace DEVinCar.Service.DTOs
 {
@@ -10,5 +11,18 @@ namespace DEVinCar.Service.DTOs
         public string Name { get; internal set; }
         public DateTime BirthDate { get; internal set; }
         public Permissions Role { get; set; }
+
+        public LoginDTO()
+        {
+        }
+        public LoginDTO(User user)
+        {
+            Id = user.Id;
+            Email = user.Email;
+            Password = user.Password;
+            Name = user.Name;
+            BirthDate = user.BirthDate;
+            Role = user.Role;
+        }
     }
 }
