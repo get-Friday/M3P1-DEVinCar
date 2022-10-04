c1aa3daae07beda66c076e698887dda968cd45e0
get.Friday <gabriel.getdev@gmail.com>
2022-10-03 21:09:44 -0300

    Feat: Cria AuthenticationController

---
+++ DEVinCar.Controller/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using DEVinCar.Service.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEVinCar.Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        public IActionResult Login(
+            [FromBody] LoginDTO body
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
