45034353daee5737710730b81f9b9665b9e42eca
get.Friday <gabriel.getdev@gmail.com>
2022-10-03 21:06:28 -0300

    Feat: Cria DTO para Login

---
+++ DEVinCar.Service/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using DEVinCar.Service.Enums;
+
+namespace DEVinCar.Service.DTOs
+{
+    public class LoginDTO
+    {
+        public int Id { get; internal set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; internal set; }
+        public DateTime BirthDate { get; internal set; }
+        public Permissions Role { get; set; }
+    }
+}
