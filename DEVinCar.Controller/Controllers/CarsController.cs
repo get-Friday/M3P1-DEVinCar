c7d2696a02644e85f99b59eda72c6ce5f98daf7e
get.Friday <gabriel.getdev@gmail.com>
2022-10-04 17:02:28 -0300

    Feat: Implementa memoryCache no CarsController em GetById, Delete e Put

--- DEVinCar.Controller/Controllers/CarsController.cs
+++ DEVinCar.Controller/Controllers/CarsController.cs
@@ -2,6 +2,7 @@ using DEVinCar.Service.DTOs;
 using DEVinCar.Service.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DEVinCar.Controller.Controllers;
 
@@ -11,10 +12,12 @@ namespace DEVinCar.Controller.Controllers;
 public class CarController : ControllerBase
 {
     private readonly ICarService _carService;
+    private readonly IMemoryCache _memoryCache;
 
-    public CarController(ICarService carService)
+    public CarController(ICarService carService, IMemoryCache memoryCache)
     {
         _carService = carService;
+        _memoryCache = memoryCache;
     }
 
     [HttpGet("{carId}")]
@@ -22,7 +25,13 @@ public class CarController : ControllerBase
         [FromRoute] int carId
     )
     {
-        return Ok(_carService.GetById(carId));
+        if (!_memoryCache.TryGetValue($"car:{carId}", out CarDTO car))
+        {
+            car = _carService.GetById(carId);
+            _memoryCache.Set($"car:{carId}", car, new TimeSpan(0, 5, 0));
+        }
+
+        return Ok(car);
     }
 
     [HttpGet]
@@ -49,6 +58,7 @@ public class CarController : ControllerBase
         [FromRoute] int carId
     )
     {
+        _memoryCache.Remove($"car:{carId}");
         _carService.Delete(carId);
         return NoContent();
     }
@@ -61,6 +71,7 @@ public class CarController : ControllerBase
     {
         carDto.Id = carId;
         _carService.Alter(carDto);
+        _memoryCache.Set($"car:{carId}", carDto, new TimeSpan(0, 5, 0));
         return NoContent();
     }
 }
