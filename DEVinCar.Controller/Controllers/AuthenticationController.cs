8f6bd5b35e1064931cc096dc8f7acd9c590d5456
get.Friday <gabriel.getdev@gmail.com>
2022-10-03 21:47:40 -0300

    Feat: Implementa endpoints em AuthenticationController

--- DEVinCar.Controller/Controllers/AuthenticationController.cs
+++ DEVinCar.Controller/Controllers/AuthenticationController.cs
@@ -1,5 +1,9 @@
 ﻿using DEVinCar.Service.DTOs;
+using DEVinCar.Service.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
 
 namespace DEVinCar.Controller.Controllers
 {
@@ -7,11 +11,35 @@ namespace DEVinCar.Controller.Controllers
     [Route("[controller]")]
     public class AuthenticationController : ControllerBase
     {
-        public IActionResult Login(
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+        public AuthenticationController(IUserService userService, ITokenService tokenService)
+        {
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public IActionResult UserLogin(
             [FromBody] LoginDTO body
         )
         {
-            throw new NotImplementedException();
+            var user = _userService.Login(body.Email, body.Password);
+            var token = _tokenService.GenerateTokenFromUser(user);
+            return Ok(new { token });
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [Authorize(Roles = "Gerente")]
+        public IActionResult Register(
+            [FromBody] UserDTO body
+        )
+        {
+            _userService.Post(body);
+            return Created("register", body);
         }
     }
 }
